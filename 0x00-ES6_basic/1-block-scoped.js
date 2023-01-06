e5a727a24a615c2f08010763dd5aa95d7ff4843d
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-06 20:17:08 +0000

    all const statements

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // Nothing goin on here
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];

a2776bedd145b704bdf179ef1aeb39fecb8fff02
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-06 20:27:21 +0000

    task 3

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,5 +1,5 @@
-import getNeighborhoodsList from './2-arrow.js';
+import getSumOfHoods from './3-default-parameter.js';
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));

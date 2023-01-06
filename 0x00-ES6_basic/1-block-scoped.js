0ea44dfd53241489a93fb8b9504404580932102f
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-06 20:14:24 +0000

    possible t1, removed code inside if statement

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // Nothing goin on here
+  }
+
+  return [task, task2];
+}
--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,3 +1,4 @@
-import { taskFirst, taskNext } from './0-constants.js';
+import taskBlock from './1-block-scoped.js';
 
-console.log(`${taskFirst()} ${taskNext()}`);
+console.log(taskBlock(true));
+console.log(taskBlock(false));

78e791162b725f81e6e2f06d0390f389657cbf9e
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-06 20:22:53 +0000

    task 2!

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,4 +1,5 @@
-import taskBlock from './1-block-scoped.js';
+import getNeighborhoodsList from './2-arrow.js';
 
-console.log(taskBlock(true));
-console.log(taskBlock(false));
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);

c595da895029deeec3b3312de9ac3d1f3c852bac
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-06 20:50:56 +0000

    task 6

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,3 +1,3 @@
-import concatArrays from './5-spread-operator.js';
+import getSanFranciscoDescription from './6-string-interpolation.js';
 
-console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
+console.log(getSanFranciscoDescription());

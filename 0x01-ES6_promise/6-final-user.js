d6a0fd261de5e6a430b91845ed78b49a50ab2f43
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-11 20:52:09 +0000

    task 6?

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,6 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all(signUpUser[(firstName, lastName), uploadPhoto(fileName)]);
+}

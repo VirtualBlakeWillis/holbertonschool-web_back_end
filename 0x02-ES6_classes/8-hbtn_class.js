ba3c566c33a49bd4faad0b0cebe4295575cc875d
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-12 01:54:12 +0000

    taask 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+  [Symbol.toPrimitive](type) {
+    if (type === 'number') {
+      return (this._size);
+    }
+    if (type === 'string') {
+      return (this._location);
+    }
+    return null;
+  }
+}

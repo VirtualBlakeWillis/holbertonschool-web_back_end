660d263fba7c5ce701b8e614cfef6db528dd95f3
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-17 17:48:08 +0000

    throws error properly

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,11 @@
 export default function updateUniqueItems(map) {
-  return map.forEach((value, key) => {
-    if (value == 1) {
-      map.set(key, 100);
-    }
-    return map.get(key);
-  })
+  if (map instanceof Map) {
+    return map.forEach((value, key) => {
+      if (value == 1) {
+        map.set(key, 100);
+      }
+      return map.get(key);
+    })
+  }
+  throw new Error('Cannot process');
 }

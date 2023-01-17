f2b59f95ad64440645f87c77c4d871a460e27ad1
VirtualBlakeWillis <bewillis101@gmail.com>
2023-01-17 17:26:34 +0000

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-continue */
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') {
     return '';
   }
   let result = '';
   for (const str of set) {
+    if (typeof str !== 'string') {
+      continue;
+    }
     if (str.startsWith(startString)) {
       if (result.length > 0) {
         result += '-';
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,4 +1,4 @@
 import cleanSet from "./8-clean_set.js";
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']), 'id-'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));

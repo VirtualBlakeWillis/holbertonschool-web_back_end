e9198912d448bd656717303c48b23c5986b4c0fc
virtualblakewillis <bewillis101@gmail.com>
2023-05-08 13:34:08 -0500

    t5, correct import

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Basic annotations - sum_list"""
-
-
 from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
     """Return the sum of all elements of a list"""
     return sum(input_list)

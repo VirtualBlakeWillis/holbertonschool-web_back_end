550771fe68e4e4e9f6f6638ba8a2f5813debbdd9
virtualblakewillis <bewillis101@gmail.com>
2023-05-08 13:37:47 -0500

    t7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Complex types - string and int/float to tuple"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple of (string, float)"""
+    return (k, v * v)

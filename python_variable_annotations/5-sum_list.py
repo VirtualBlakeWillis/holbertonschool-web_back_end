73bb5f8094c94c3256f8568dde442f3102503ead
virtualblakewillis <bewillis101@gmail.com>
2023-05-08 13:31:55 -0500

    t5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - sum_list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return the sum of all elements of a list"""
+    return sum(input_list)

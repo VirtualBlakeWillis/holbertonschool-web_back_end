695f9fb23a8cab6552bf0ca6b9f33299ec51eae3
virtualblakewillis <bewillis101@gmail.com>
2023-05-09 14:00:19 -0500

    i should start testing locally

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Args:
         n (int): Number of times to spawn wait_random.

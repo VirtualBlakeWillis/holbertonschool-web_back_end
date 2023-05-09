6da2493e6df32e2f493227b51d66e2b1fa09e771
virtualblakewillis <bewillis101@gmail.com>
2023-05-09 10:17:53 -0500

    t9, pt 2

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Complex types - given a function, annotate its types and returns"""
+from typing import List, Tuple
 
-
-def element_length(lst: List) -> List[Tuple[str, int]]:
+def element_length(lst: List) -> Tuple[str, int]:
     """Return a list of tuples containing elements and their length"""
     return [(i, len(i)) for i in lst]

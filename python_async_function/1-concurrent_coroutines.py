414384f7a55357a37756f863bb532314debf1ba0
virtualblakewillis <bewillis101@gmail.com>
2023-05-09 12:54:20 -0500

    type annotations

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,11 +6,12 @@ n and max_delay. You will spawn wait_random n times with the specified max_delay
 wait_n should return the list of all the delays (float values). The list of the
 delays should be in ascending order without using sort() because of concurrency.
 """
+from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Args:
         n (int): Number of times to spawn wait_random.

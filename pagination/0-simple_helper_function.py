0e46d54ad99ebbd0a519c901027036990becc699
virtualblakewillis <bewillis101@gmail.com>
2023-05-16 14:15:13 -0500

    pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Write a function named index_range that takes two integer arguments page and page_size. #noqa
+Write a function named index_range that takes two integer arguments
+page and page_size.
 
 The function should return a tuple of size two containing a start index and
 an end index corresponding to the range of indexes to return in a list for

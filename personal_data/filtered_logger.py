75db48378ec492b76e3b28d1d54f0771b4f388a7
virtualblakewillis <bewillis101@gmail.com>
2023-05-24 14:03:33 -0500

    indenting in tabs, why is wrong with pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -15,28 +15,28 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-	""" Redacting Formatter class"""
+    """ Redacting Formatter class"""
 
-	REDACTION = "***"
-	FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-	SEPARATOR = ";"
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-	def __init__(self, fields: List[str]):
-		super(RedactingFormatter, self).__init__(self.FORMAT)
-		self.fields = fields
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
-	def format(self, record: logging.LogRecord) -> str:
-		""" Filters values in incoming log records using filter_datum """
-		return filter_datum(self.fields, self.REDACTION,
-							super().format(record), self.SEPARATOR)
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-				 separator: str) -> str:
-	"""
-	Returns the log message obfuscated
-	"""
-	for field in fields:
-		message = re.sub(field + "=.*?" + separator,
-						 field + "=" + redaction + separator, message)
-	return message
+                 separator: str) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message

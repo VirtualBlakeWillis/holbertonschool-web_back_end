9c4cdb5a1b2495364f19b924bcf77689032f5f49
virtualblakewillis <bewillis101@gmail.com>
2023-05-29 15:32:48 -0500

    t6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """ Encryption """
 import bcrypt
 
@@ -5,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string. """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates that the provided password matches the hashed password. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

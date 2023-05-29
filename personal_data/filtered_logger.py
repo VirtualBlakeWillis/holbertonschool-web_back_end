81eb4f460d38c44094ea99603c91be4d294837dd
virtualblakewillis <bewillis101@gmail.com>
2023-05-29 12:46:39 -0500

    correct type annotation?

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ The function should use a regex to replace occurrences of certain field values.
 filter_datum should be less than 5 lines long and use re.sub to perform the substitution with a single regex. # noqa
 """
 import re
-from typing import List, Tuple, MySQLConnection
+from typing import List, Tuple
 import logging
 import mysql
 
@@ -58,7 +58,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> MySQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Implement a get_db function that returns a connector to the database (mysql.connector.connection.MySQLConnection object) """  # noqa
     return mysql.connector.connect(
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),

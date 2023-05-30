ba015ba43d891f1b530113ca1216da6ce188d685
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:28:13 -0500

    t8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,4 +17,13 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic '):
             return authorization_header[6:]
         return None
-    pass
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # noqa
+        """ decode base64 authorization header """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64_authorization_header.decode('utf-8')
+        except Exception:
+            return None

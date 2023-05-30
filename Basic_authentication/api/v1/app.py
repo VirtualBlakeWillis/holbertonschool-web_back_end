15283f0b064ac612864dc15a714e755daed51b32
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:18:46 -0500

    changes to app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ if os.getenv("AUTH_TYPE") == "auth":
     import api.v1.auth.auth as auth
     auth = auth.Auth()
 
+if os.getenv("AUTH_TYPE") == "basic_auth":
+    import api.v1.auth.basic_auth as auth
+    auth = auth.BasicAuth()
 
 @app.before_request
 def before_request_func() -> str:

eecd07b0295a6a353378696c9a1a871a0c94ae9e
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:25:44 -0500

    # noqa, get rekt pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str: # noqa
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
         """ extract base64 authorization header """
         if authorization_header is None or type(authorization_header) != str:
             return None

3d64b3052e584143a0c30ddfb429daaac7b34af1
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:23:18 -0500

    t7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ BasicAuth class """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ extract base64 authorization header """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header.startswith('Basic '):
+            return authorization_header[6:]
+        return None
     pass

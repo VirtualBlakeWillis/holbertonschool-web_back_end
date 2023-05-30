65403efa6fa646276252fa7de7038abc8478efe1
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 16:10:59 -0500

    user.to_json instead of requests.current_user

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -33,7 +33,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me' and request.current_user is None:
         abort(404)
     if user_id == 'me' and request.current_user is not None:
-        return jsonify(request.current_user.to_json())
+        return jsonify(user.to_json())
     return jsonify(user.to_json())
 
 

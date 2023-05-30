7b40a62b6f235a32db02c75ce4b6c24ea2cd8d31
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:36:53 -0500

    i <3 noqa

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,7 +30,7 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):  # noqa
         """ extract user credentials """
-        if decoded_base64_authorization_header is None or type(decoded_base64_authorization_header) != str:
+        if decoded_base64_authorization_header is None or type(decoded_base64_authorization_header) != str:  # noqa
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None

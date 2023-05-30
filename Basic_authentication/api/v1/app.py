1451385a0639e09616dac52db599101fa8ea3c19
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 14:06:15 -0500

    t5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,28 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+    import api.v1.auth.auth as auth
+    auth = auth.Auth()
+
+
+@app.before_request
+def before_request_func() -> str:
+    """ before request function
+    """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                        '/api/v1/unauthorized/',
+                        '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 
 @app.errorhandler(404)
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """

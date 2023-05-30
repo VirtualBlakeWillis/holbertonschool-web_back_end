9d5aa8a7e26db09462fe8833ebfa9b749cab6b67
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:33:41 -0500

    t8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ from flask import request
 from typing import List, TypeVar
 from os import getenv
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,10 +21,9 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # noqa
         """ decode base64 authorization header """
-        if base64_authorization_header is None or \
-                type(base64_authorization_header) != str:
+        if base64_authorization_header is None or type(base64_authorization_header) != str:  # noqa
             return None
         try:
-            return base64_authorization_header.decode('utf-8')
+            return base64.b64decode(base64_authorization_header).decode('utf-8')  # noqa
         except Exception:
             return None

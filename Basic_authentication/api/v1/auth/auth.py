68bcd482a6245cd40187bc51a5e00ce54d7a7bd7
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 14:00:41 -0500

    i dare you to not work

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,21 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth """
-        return False
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        for excluded in excluded_paths:
+            if excluded.endswith('*'):
+                if path.startswith(excluded[:-1]):
+                    return False
+            elif path == excluded:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """

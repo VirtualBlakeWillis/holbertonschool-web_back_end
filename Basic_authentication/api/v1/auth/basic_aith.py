eb37ba9efa67a49a7354ba456335edca9b3226ac
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 15:13:52 -0500

    class documentation

---
+++ Basic_authentication/api/v1/auth/basic_aith.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Module for user authentication """
+
+from flask import request
+from typing import List, TypeVar
+from os import getenv
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class """
+    pass

0150db789fed707af568bb753dab93abd73e0545
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 11:49:02 -0500

    IndentionError

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -16,7 +16,7 @@ def login() -> str:
     if not password or password == '':
         return jsonify({"error": "password missing"}), 400
 
-     user = User.search({'email': email})
+    user = User.search({'email': email})
 
     if not user:
         return jsonify({"error": "no user found for this email"}), 404

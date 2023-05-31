cb1fb92edcf8294ca532528816a2337e9735a402
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:26:26 -0500

    t18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -83,3 +83,16 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Method that updates a user password
+        """
+        if reset_token is None or password is None:
+            return None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_password)
+        self._db.update_user(user.id, reset_token=None)

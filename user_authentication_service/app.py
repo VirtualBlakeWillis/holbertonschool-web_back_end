96e3037f2f40653123ac9ffe8716a5e954f1c429
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:18:25 -0500

    t11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -33,5 +33,23 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions
+    Login
+    Return:
+      - Session ID JSON
+    """
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

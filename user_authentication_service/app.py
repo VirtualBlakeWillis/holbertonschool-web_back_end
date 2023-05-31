cfc02bc2783c4027ca7fc0d9e61e543200f92d96
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:05:23 -0500

    t6

---
+++ user_authentication_service/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" flask app """
+
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """ GET /
+    Return:
+      - welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")

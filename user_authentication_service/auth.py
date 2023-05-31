6e98502dbc849fb4726e62c96d89dde3c9f9d641
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:03:31 -0500

    t5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -12,3 +12,20 @@ def _hash_password(password: str) -> str:
     """ Method that takes in a password string arguments and returns bytes
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method that registers and returns a new user if email isn't listed
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError(f'User {email} already exists')

e4a3198c84352f6e51e3cada6cda0dd24f127cf3
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 12:59:03 -0500

    i will burn imports to the ground

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 

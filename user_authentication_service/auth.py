b39ffbafbc2cadb5ab8e3b3c6bf8460e83d6a6e2
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:04:01 -0500

    pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,6 +13,7 @@ def _hash_password(password: str) -> str:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """

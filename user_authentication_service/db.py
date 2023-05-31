2b6757b5e7866c4ffe0ed1a2c0f132766d996b13
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 12:55:48 -0500

    fixed imports

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -3,6 +3,7 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 

9417f20efe83dc2d985cb3bb4d174c1dff5ab858
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 21:04:34 -0500

    t2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,4 +8,13 @@ import uuid
 
 class SessionAuth(Auth):
     """ SessionAuth class """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a session_id for a user_id """
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

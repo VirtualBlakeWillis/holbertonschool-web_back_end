b28fdd0cdaea4a950efa760e05eba744c953dc53
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 12:50:31 -0500

    cmon holbie gang

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    @property
+
     def add_user(self, email: str, hashed_password: str) -> None:
         """Method that saves a new user to the database
         """

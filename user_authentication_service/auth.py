b6d67fccc58b73373eb691af7df80cc0f924b0b5
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:40:12 -0500

    pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -96,3 +96,4 @@ class Auth:
         hashed_password = _hash_password(password)
         self._db.update_user(user.id, hashed_password=hashed_password)
         self._db.update_user(user.id, reset_token=None)
+

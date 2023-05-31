f63ae23a0ee9f7caad1ce63e55b0fb472fa9abb6
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:25:11 -0500

    t16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -72,3 +72,14 @@ class Auth:
         """ Method that deletes the user session / logout
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Method that generates a reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token

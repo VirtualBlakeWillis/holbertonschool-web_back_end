7d063a8e1c03fe43c615ef4f55d6aae9513e06da
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:19:20 -0500

    t12

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Method that returns a user ID based on a session ID
+        """
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None

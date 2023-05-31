2bcc7b5a21f222018e99c0b9337b6eaff0f546bf
virtualblakewillis <bewillis101@gmail.com>
2023-05-30 21:02:26 -0500

    fixed :D

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -34,11 +34,9 @@ def before_request_func() -> str:
                       '/api/v1/forbidden/']
 
     if auth and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None\
-                and auth.session_cookie(request) is None:
+        if auth.authorization_header(request) is None:
             abort(401)
-        if auth.authorization_header(request) is None\
-                and auth.session_cookie(request):
+        if auth.authorization_header(request) is None:
             abort(403)
         if auth.current_user(request) is None:
             abort(403)

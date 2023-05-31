353e5d1c09d80956cfb33410b9647ab80d2738c1
virtualblakewillis <bewillis101@gmail.com>
2023-05-31 13:21:21 -0500

    t14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -51,5 +51,21 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /sessions
+    Logout
+    Return:
+      - Destroy Session ID
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

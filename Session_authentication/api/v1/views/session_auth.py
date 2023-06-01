38afd0267f53305b08eedf60205291a991f5f002
virtualblakewillis <bewillis101@gmail.com>
2023-06-01 11:33:25 -0500

    YAY

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Module for Session Authentication views """
 from api.v1.views import app_views
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
+
 
 
 
@@ -20,15 +21,16 @@ def login() -> str:
 
     user_list = User.search({'email': email})
 
-    if user_list is []:
+    if not user_list:
         return jsonify({"error": "no user found for this email"}), 404
 
     if not user_list[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-    res = jsonify(user.to_json())
-    session_id = auth.create_session(user.id)
+    res = jsonify(user_list[0].to_json())
+    session_id = auth.create_session(user_list[0].id)
     cookie_name = getenv('SESSION_NAME')
+
     res.set_cookie(cookie_name, session_id)
     return res
--- Session_authentication/main_0.py
+++ Session_authentication/main_0.py
@@ -1,36 +1,35 @@
-#!/usr/bin/python3
-""" Check response
+#!/usr/bin/env python3
+""" Main 4
 """
-import requests
-import base64
+from flask import Flask, request
+from api.v1.auth.session_auth import SessionAuth
+from models.user import User
+
+""" Create a user test """
+user_email = "bobsession@hbtn.io"
+user_clear_pwd = "fake pwd"
+
+user = User()
+user.email = user_email
+user.password = user_clear_pwd
+user.save()
+
+""" Create a session ID """
+sa = SessionAuth()
+session_id = sa.create_session(user.id)
+print("User with ID: {} has a Session ID: {}".format(user.id, session_id))
+
+""" Create a Flask app """
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    request_user = sa.current_user(request)
+    if request_user is None:
+        return "No user found\n"
+    return "User found: {}\n".format(request_user.id)
 
 if __name__ == "__main__":
-    user_email = "u4@hbtn.io"
-    user_pwd = "pwd4"
-
-    r = requests.post('http://0.0.0.0:5000/api/v1/auth_session/login', data={ 'email': user_email, 'password': "wrong pwd" })
-    if r.status_code != 401:
-        print("Wrong status code: {}".format(r.status_code))
-        exit(1)
-    if r.headers.get('content-type') != "application/json":
-        print("Wrong content type: {}".format(r.headers.get('content-type')))
-        exit(1)
-    
-    try:
-        r_json = r.json()
-        
-        if len(r_json.keys()) != 1:
-            print("Not the right number of element in the JSON: {}".format(r_json))
-            exit(1)
-        
-        error_value = r_json.get('error')
-        if error_value is None:
-            print("Missing 'error' key in the JSON: {}".format(r_json))
-            exit(1)
-        if error_value != "wrong password":
-            print("'error' doesn't have the right value: {}".format(error_value))
-            exit(1)
-            
-        print("OK", end="")
-    except:
-        print("Error, not a JSON")
+    app.run(host="0.0.0.0", port="5000")

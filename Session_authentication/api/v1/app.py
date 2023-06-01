ad24ab4ef5f1bf26d3e6f403d54e0e2d1ce7a0b3
virtualblakewillis <bewillis101@gmail.com>
2023-06-01 11:41:36 -0500

    i hate this, work please

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -38,8 +38,8 @@ def before_request_func() -> str:
         if auth.authorization_header(request) is None and \
               auth.session_cookie(request) is None:
             abort(401)
-        if auth.authorization_header(request) is None:
-            abort(403)
+        # if auth.authorization_header(request) is None:
+        #     abort(403)
         if auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)

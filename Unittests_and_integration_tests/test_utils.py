6a54da242c20b878e43e56a46696d9c96ad2e2e5
virtualblakewillis <bewillis101@gmail.com>
2023-06-05 11:35:44 -0500

    task 0, thanks buzz :D

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Unittests and Integration tests for the utils.py file.
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ task 0 """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ tests access_nested_map """
+        self.assertEqual(access_nested_map(nested_map, path), expected)

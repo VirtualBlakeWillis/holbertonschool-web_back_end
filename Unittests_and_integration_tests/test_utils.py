9880134cdb23f29bc64278eb87d0a85b5beca84b
virtualblakewillis <bewillis101@gmail.com>
2023-06-05 13:15:50 -0500

    t3?

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -41,3 +41,25 @@ class TestGetJson(unittest.TestCase):
         """ tests get_json """
         mock_get.return_value.json.return_value = test_payload
         self.assertEqual(get_json(test_url), mock_get.return_value.json.return_value)
+
+
+TestMemoize(unittest.TestCase):
+    """ task 3 """
+
+    def test_memoize(self):
+
+        class TestClass:
+            """ test class """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test = TestClass()
+            test.a_property
+            test.a_property
+            mock_method.assert_called_once()

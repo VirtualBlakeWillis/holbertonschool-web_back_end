a872a23b90f370a2826b27687c5c69d6f0ac6990
virtualblakewillis <bewillis101@gmail.com>
2023-06-05 11:37:14 -0500

    t1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ tests access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ tests access_nested_map exception """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
+

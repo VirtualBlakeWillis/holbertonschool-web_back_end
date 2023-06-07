4569023f1304345e0fa82cbdf3239fe56d62e8de
virtualblakewillis <bewillis101@gmail.com>
2023-06-07 13:11:50 -0500

    t1

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -3,9 +3,6 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
-
-
 @app.route('/')
 def hello():
     """ Return welcome message """
---
+++ i18n/1-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Basic Flask app - task 0 """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config(object):
+    """ Babel config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """ Return welcome message """
+    return render_template('0-index.html')

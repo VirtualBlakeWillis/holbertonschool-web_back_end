2200c1e28a703a3b073e39bf416b85a4c25bb933
virtualblakewillis <bewillis101@gmail.com>
2023-06-07 13:12:37 -0500

    pYcOdEsTyLe

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -6,11 +6,13 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """ Babel config class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 

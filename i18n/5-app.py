161b949f35c05927ccc9451b73954a82eabca980
virtualblakewillis <bewillis101@gmail.com>
2023-06-08 10:37:13 -0500

    t5

---
+++ i18n/5-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Basic Flask app - task 0 """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Babel config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+@app.route('/')
+def hello():
+    """ Return welcome message """
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale language """
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+def get_user():
+    """ Get user """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+@app.before_request
+def before_request():
+    """ Before request """
+    flask.g.user = get_user()

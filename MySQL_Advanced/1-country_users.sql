8945313a74f737e5b92f61dfae97e652c819fda2
VirtualBlakeWillis <bewillis101@gmail.com>
2023-06-19 16:47:56 +0000

    put code into correct file

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY (id)
     );

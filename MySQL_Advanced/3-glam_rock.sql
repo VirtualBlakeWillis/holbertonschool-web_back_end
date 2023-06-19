6b5da99c6e02be3e1d7455abfc56aa1332f67e9c
VirtualBlakeWillis <bewillis101@gmail.com>
2023-06-19 17:57:59 +0000

    2020, not 2021

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- my task 3
-SELECT band_name, (COALESCE(split, 2021) - formed) AS lifespan
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style) > 0
 ORDER BY lifespan DESC;

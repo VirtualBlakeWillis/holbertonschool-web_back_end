293a67873d2cc1c51fd42a3153ad32d7c41dd3d5
VirtualBlakeWillis <bewillis101@gmail.com>
2023-06-19 16:42:54 +0000

    unique email

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Write a SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     PRIMARY KEY (id)
     );

e662fbfc1ea3633d002aab46b73cb11281ebbe6f
VirtualBlakeWillis <bewillis101@gmail.com>
2023-06-19 16:39:36 +0000

    fixed mySQL syntax

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Write a SQL script that creates a table users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id int,
     email varchar(255) NOT NULL,
     name varchar(255),
     PRIMARY KEY (id)
-    ) IF NOT EXISTS;
+    );

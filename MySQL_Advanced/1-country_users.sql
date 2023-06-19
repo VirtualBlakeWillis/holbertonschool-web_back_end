15beddeb2784d8210c902ad6051d520ab66e1471
VirtualBlakeWillis <bewillis101@gmail.com>
2023-06-19 16:44:49 +0000

    t1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY (id)
     );

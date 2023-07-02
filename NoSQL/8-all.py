35ef0a8acb90388cf72324548bec455b8aa95983
VirtualBlakeWillis <bewillis101@gmail.com>
2023-07-02 12:39:49 -0700

    documentation

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,6 +3,7 @@
 
 
 def list_all(mongo_collection):
+    """ function that lists all documents in a collection """
     documents = []
     for document in mongo_collection.find():
         documents.append(document)

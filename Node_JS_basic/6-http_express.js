30b55bf50dcb08697e8a6a12c4b245278448a4bd
virtualblakewillis <bewillis101@gmail.com>
2023-07-05 14:22:09 -0600

    t6, express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+// simple express server
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;

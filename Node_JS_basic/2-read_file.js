685de735a70ac9cdbda4afb44338495b41e882f1
virtualblakewillis <bewillis101@gmail.com>
2023-07-05 12:49:51 -0600

    correctly counts student

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,12 +9,13 @@ CSV file can contain empty lines (at the end) - and they are not a valid student
   const fs = require('fs');
   
   fs.readFile(path, 'utf8', (err, data) => {
-    // If the database is not available, it should throw an error with the text Cannot load the database
+    // error if not avalible
     if (err) throw new Error('Cannot load the database');
+
     // If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
     const lines = data.split('\n');
     const students = lines.filter((line) => line).map((line) => line.split(','));
-    const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+    const NUMBER_OF_STUDENTS = students.length ? students.length - 2 : 0;
     console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
     // It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
     const fields = {};

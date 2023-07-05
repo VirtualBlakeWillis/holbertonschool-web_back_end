072f63d5c1fad5c58f3ec54c6d89fb606238b760
virtualblakewillis <bewillis101@gmail.com>
2023-07-05 14:13:04 -0600

    eslint?

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,29 +1,57 @@
-// creating a simple HTTP server using Node's HTTP module
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const url = require('url');
+const fs = require('fs').promises;
 
-const port = 1245;
-// use local host
-const host = '127.0.0.1';
+function countStudents(path) {
+  let result = '';
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      // split the data into rows and remove empty lines
+      const rows = data.split('\n').filter((row) => row);
+
+      // find index of field and firstName in csv
+      const headers = rows.shift().split(',');
+      const fieldIndex = headers.indexOf('field');
+      const firstNameIndex = headers.indexOf('firstname');
+
+      // retrieve only the fields, without repeats
+      const fields = [...new Set(rows.map((row) => row.split(',')[fieldIndex]))];
+
+      result += `Number of students: ${rows.length}\n`;
+
+      fields.forEach((field) => {
+        // split rows and retrieve field; if this matches current field, store
+        // in students
+        const students = rows.filter((row) => row.split(',')[fieldIndex] === field);
+        // retrieve names and print them comma-separated
+        result += `Number of students in ${field}: ${students.length}. List: ${students.map((student) => student.split(',')[firstNameIndex]).join(', ')}\n`;
+      });
+      return result;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const q = url.parse(req.url, true).path;
+  if (q === '/') {
     res.write('Hello Holberton School!');
     res.end();
   }
-
-  if (req.url === '/students') {
+  if (q === '/students') {
     res.write('This is the list of our students\n');
-    countStudents(process.argv[2])
+    countStudents(process.argv[2], res)
       .then((data) => {
-        res.end(data);
+        res.write(data);
+        res.end();
       })
       .catch((error) => {
-        res.end(error.message);
+        res.write(error.message);
+        res.end();
       });
   }
-});
-
-app.listen(port, host);
+}).listen(1245);
 
 module.exports = app;

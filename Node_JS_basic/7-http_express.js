86b84d283c47d5ee139faea7d724acaea50743b0
virtualblakewillis <bewillis101@gmail.com>
2023-07-05 14:26:19 -0600

    t7, fixed output to be beautiful

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
+  let result = '';
   fs.readFile(process.argv[2], 'utf8')
     .then((data) => {
       const rows = data.split('\n').filter((row) => row);
@@ -16,7 +17,8 @@ app.get('/students', (req, res) => {
       const fieldIndex = headers.indexOf('field');
       const firstNameIndex = headers.indexOf('firstname');
       const fields = [...new Set(rows.map((row) => row.split(',')[fieldIndex]))];
-      let result = `Number of students: ${rows.length}\n`;
+      result += 'This is the list of our students\n';
+      result += `Number of students: ${rows.length}\n`;
       fields.forEach((field) => {
         const students = rows.filter((row) => row.split(',')[fieldIndex] === field);
         result += `Number of students in ${field}: ${students.length}. List: ${students.map((student) => student.split(',')[firstNameIndex]).join(', ')}\n`;

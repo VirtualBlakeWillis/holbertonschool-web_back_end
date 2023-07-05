0231985c504033dd2f2aeea1a28835a4babb3729
virtualblakewillis <bewillis101@gmail.com>
2023-07-05 13:23:31 -0600

    mostly works

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,28 +1,40 @@
 function countStudents(path) {
-  /*
-  The script should attempt to read the database file synchronously
-If the database is not available, it should throw an error with the text Cannot load the database
-If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
-It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
-CSV file can contain empty lines (at the end) - and they are not a valid student!
-  */
+
   const fs = require('fs');
-  
+
   fs.readFile(path, 'utf8', (err, data) => {
     // error if not avalible
     if (err) throw new Error('Cannot load the database');
 
-    // If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
-    const lines = data.split('\n');
-    const students = lines.filter((line) => line).map((line) => line.split(','));
-    const NUMBER_OF_STUDENTS = students.length ? students.length - 2 : 0;
-    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
-    // It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
+    const rows = data.split('\n').filter((row) => row);
+    const headers = rows.shift().split(',');
+    const fieldIndex = headers.indexOf('field');
+    const firstNameIndex = headers.indexOf('firstname');
+    // number of studdents
+    console.log(`Number of students: ${rows.length}`);
+
+    // log the number of students in each field, with the following format:
+    // Number of students in <field>: <number>. List: <firstname1>, <firstname2>, ...
     const fields = {};
-    for (const student of students) {
-      if (!fields[student[3]]) fields[student[3]] = [];
-      fields[student[3]].push(student[0]);
+    rows.forEach((row) => {
+      const field = row.split(',')[fieldIndex];
+      const firstName = row.split(',')[firstNameIndex];
+      if (!fields[field]) fields[field] = {
+        count: 0,
+        students: [],
+      };
+      fields[field].count += 1;
+      fields[field].students.push(firstName);
     }
+    );
+    for (const field in fields) {
+      if (field) {
+        const n = fields[field].count;
+        const names = fields[field].students.join(', ');
+        console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+      }
+    }
+
   });
 }
 

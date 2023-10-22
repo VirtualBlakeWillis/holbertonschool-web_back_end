6a8aa8f5620322fb6b746997075a463f9e55d974
virtualblakewillis <bewillis101@gmail.com>
2023-10-22 10:51:21 -0500

    rudementary task 0

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    data = fs.readFileSync(path, 'utf8');
+
+    lines = data.split('\n');
+    lines = lines.slice(1, lines.length - 1);
+    console.log(`Number of students: ${lines.length}`);
+    let students = {};
+    for (const line of lines) {
+      const student = line.split(',');
+      if (!students[student[3]]) {
+        students[student[3]] = [];
+      }
+      students[student[3]].push(student[0]);
+    }
+    for (const field in students) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
+    }
+  } catch {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

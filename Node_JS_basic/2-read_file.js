3603829fc25fd88e4f5b1c3edc1460546c62060c
virtualblakewillis <bewillis101@gmail.com>
2023-10-22 11:20:08 -0500

    rewrote function, probably will not fix error

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-
-    let lines = data.split('\n');
-    lines = lines.slice(1, lines.length);
-    console.log(`Number of students: ${lines.length}`);
+    const lines = data.split('\n').slice(1);
     const students = {};
+    let studentCount = 0;
     for (const line of lines) {
       const student = line.split(',');
-      if (!students[student[3]]) {
-        students[student[3]] = [];
+      if (student[0] !== '') {
+        if (!students[student[3]]) {
+          students[student[3]] = [];
+        }
+        students[student[3]].push(student[0]);
+        studentCount += 1;
       }
-      students[student[3]].push(student[0]);
     }
+    console.log(`Number of students: ${studentCount}`);
     for (const field in students) {
       if (field) {
         console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);

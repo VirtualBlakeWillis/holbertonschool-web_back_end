15b6497994251c8c913357f958b08897459ab793
virtualblakewillis <bewillis101@gmail.com>
2023-10-23 10:16:53 -0500

    task 0 and project setup

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', function() {
+  it('should return 4', function() {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('should return 5', function() {
+    assert.equal(calculateNumber(1.6, 3), 5);
+  });
+  it('should return 5', function() {
+    assert.equal(calculateNumber(1.2, 3.8), 5);
+  });
+  it('should return 5', function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+});
+
+
---
+++ unittests_in_js/README.md
@@ -0,0 +1,1 @@
+this readme is NOT empty :D

41951a1fa41e6f61430241e38820b1968c536d8c
virtualblakewillis <bewillis101@gmail.com>
2023-10-31 17:41:41 -0500

    task 3

---
+++ queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,28 @@
+import { createClient } from "redis";
+const redis = require("redis");
+
+const client = createClient();
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
+
+client.on("error", (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (error, reply) => {
+    redis.print(`Reply: ${reply}`);
+  });
+}
+
+async function displaySchoolValue(schoolName) {
+  await client.get(schoolName, (error, reply) => {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
